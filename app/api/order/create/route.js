9618ee751fbdebfbebb05946e8937a7a350022e8
Shubhamnagar21 <shubhamnagar5000@gmail.com>
2025-12-24 13:26:44 +0530

    stripe added

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -1,8 +1,9 @@
 import { inngest } from "@/config/inngest";
 import Product from "@/models/Product";
 import User from "@/models/User";
-import { getAuth} from "@clerk/nextjs/server";
+import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
+import Order from "@/models/Order";
 import toast from "react-hot-toast";
 
 export async function POST(request) {
@@ -17,27 +18,35 @@ export async function POST(request) {
     // calculate amount using items
     const amount = await items.reduce(async (acc, item) => {
       const product = await Product.findById(item.product);
-      return await acc + product.offerPrice * item.quantity;
+      return (await acc) + product.offerPrice * item.quantity;
     }, 0);
-
-    await inngest.send({
-        name: 'order/created',
-        data: {
-            userId,address,
-            items,
-            amount: amount+ Math.floor(0.02*amount),
-            date: Date.now()
-        }
-    })
+    await Order.create({
+      userId,
+      address,
+      items,
+      amount: amount + Math.floor(0.02 * amount),
+      date: Date.now(),
+      paymentType: "COD",
+    });
+    // await inngest.send({
+    //     name: 'order/created',
+    //     data: {
+    //         userId,address,
+    //         items,
+    //         amount: amount+ Math.floor(0.02*amount),
+    //         date: Date.now(),
+    //         paymentType: "COD",
+    //     }
+    // })
+  
     // clear user cart
-    const user = await User.findById(userId)
-    user.cartItem = {}
-    await user.save()
+    const user = await User.findById(userId);
+    user.cartItem = {};
+    await user.save();
 
-    return NextResponse.json({success : true, message: 'Order Placed'})
-
+    return NextResponse.json({ success: true, message: "Order Placed" });
   } catch (error) {
-    console.log(error)
-    return NextResponse.json({success: false, message: error.message})
+    console.log(error);
+    return NextResponse.json({ success: false, message: error.message });
   }
 }
--- components/OrderSummary.jsx
+++ components/OrderSummary.jsx
@@ -68,6 +68,44 @@ const OrderSummary = () => {
     }
   }
 
+  const createOrderStripe = async()=>{
+    try {
+      if (!selectedAddress) {
+        return toast.error("Please select an address");
+      }
+
+      let cartItemsArray = Object.keys(cartItems).map((key) => ({
+        product: key,
+        quantity: cartItems[key],
+      }));
+      cartItemsArray = cartItemsArray.filter((item) => item.quantity > 0);
+
+      if (cartItemsArray.length === 0) {
+        return toast.error("Cart is empty");
+      }
+      const token = await getToken();
+
+      const { data } = await axios.post(
+        "/api/order/stripe",
+        {
+          address: selectedAddress._id,
+          items: cartItemsArray,
+        },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      if(data.success){
+        window.location.href = data.url; 
+      }else{
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
+
   useEffect(() => {
     if(user){
       fetchUserAddresses()
@@ -95,10 +133,21 @@ const OrderSummary = () => {
                   ? `${selectedAddress.fullName}, ${selectedAddress.area}, ${selectedAddress.city}, ${selectedAddress.state}`
                   : "Select Address"}
               </span>
-              <svg className={`w-5 h-5 inline float-right transition-transform duration-200 ${isDropdownOpen ? "rotate-0" : "-rotate-90"}`}
-                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#6B7280"
+              <svg
+                className={`w-5 h-5 inline float-right transition-transform duration-200 ${
+                  isDropdownOpen ? "rotate-0" : "-rotate-90"
+                }`}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="#6B7280"
               >
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M19 9l-7 7-7-7"
+                />
               </svg>
             </button>
 
@@ -110,7 +159,8 @@ const OrderSummary = () => {
                     className="px-4 py-2 hover:bg-gray-500/10 cursor-pointer"
                     onClick={() => handleAddressSelect(address)}
                   >
-                    {address.fullName}, {address.area}, {address.city}, {address.state}
+                    {address.fullName}, {address.area}, {address.city},{" "}
+                    {address.state}
                   </li>
                 ))}
                 <li
@@ -145,7 +195,10 @@ const OrderSummary = () => {
         <div className="space-y-4">
           <div className="flex justify-between text-base font-medium">
             <p className="uppercase text-gray-600">Items {getCartCount()}</p>
-            <p className="text-gray-800">{currency}{getCartAmount()}</p>
+            <p className="text-gray-800">
+              {currency}
+              {getCartAmount()}
+            </p>
           </div>
           <div className="flex justify-between">
             <p className="text-gray-600">Shipping Fee</p>
@@ -153,16 +206,25 @@ const OrderSummary = () => {
           </div>
           <div className="flex justify-between">
             <p className="text-gray-600">Tax (2%)</p>
-            <p className="font-medium text-gray-800">{currency}{Math.floor(getCartAmount() * 0.02)}</p>
+            <p className="font-medium text-gray-800">
+              {currency}
+              {Math.floor(getCartAmount() * 0.02)}
+            </p>
           </div>
           <div className="flex justify-between text-lg md:text-xl font-medium border-t pt-3">
             <p>Total</p>
-            <p>{currency}{getCartAmount() + Math.floor(getCartAmount() * 0.02)}</p>
+            <p>
+              {currency}
+              {getCartAmount() + Math.floor(getCartAmount() * 0.02)}
+            </p>
           </div>
         </div>
       </div>
 
-      <button onClick={createOrder} className="w-full bg-orange-600 text-white py-3 mt-5 hover:bg-orange-700">
+      <button
+        onClick={createOrderStripe}
+        className="w-full bg-orange-600 text-white py-3 mt-5 hover:bg-orange-700"
+      >
         Place Order
       </button>
     </div>
